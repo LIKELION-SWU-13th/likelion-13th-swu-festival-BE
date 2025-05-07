41109ee3382b38caa0d41047009cc0d462dd8484
Seoheeuhm <seohee030128@naver.com>
2025-05-07 17:38:56 +0900

    init

---
+++ src/main/java/com/example/likelion_13th_festival_BE/Likelion13thFestivalBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.likelion_13th_festival_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Likelion13thFestivalBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Likelion13thFestivalBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/likelion_13th_festival_BE/Likelion13thFestivalBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.likelion_13th_festival_BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Likelion13thFestivalBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
